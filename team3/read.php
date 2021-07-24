549573db37a3c49197a88d83e318410212781d50
wldks52 <62795614+wldks52@users.noreply.github.com>
2021-07-24 17:11:42 +0900

    Add files via upload

---
+++ team3/read.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>게시판 읽기</title>
+  <link rel='stylesheet' href='bstyle.css'>
+  <script>
+    function modify(num){   //글수정
+      location.href="update.php?num="+num;
+    }
+    function delview(){
+      document.getElementById('frm1').style.display="inline";
+      document.getElementById('span1').style.visibility="hidden";
+    }
+    function cancel(){
+      document.getElementById('frm1').style.display="none";
+      document.getElementById('span1').style.visibility="";
+    }
+    function del(){        // 글삭제
+      if(document.getElementById('pwd').value==''){
+        alert('비밀번호를 입력하세요!');
+        document.getElementById('pwd').focus();
+        return;
+      }
+      document.frm1.submit();
+    }
+  </script>
+</head>
+<body>
+<?php
+  $num=$_GET['num'];
+  $conn=mysqli_connect("localhost", "edu", "1234", "sqldb") or die("MySQL 접속 실패 !!");
+  $sql="select * from board where num=".$num;
+
+  $ret = mysqli_query($conn, $sql);    
+  if($ret) {
+    //echo mysqli_num_rows($ret)."건이 조회됨<br>";
+    $row = mysqli_fetch_array($ret);
+  }else{
+    echo "데이터 읽기 실패!!!<br>";
+    echo "실패 원인 :".mysqli_error($conn);
+    exit();
+  }
+
+  echo "<h1 class='title'>상세 보기</h1>";
+  echo "<a href='list.php'>글 목록</a>&nbsp;&nbsp;";
+  echo "<a href='main.html'>처음화면</a>";
+  echo "<table>";
+  echo " <tr>";
+  echo "    <th class='write' width='120'>글번호</th>";
+  echo "    <td class='t1'>",$row['num'],"</td>";
+  echo "  </tr>";
+  echo "  <tr>";
+  echo "   <th class='write'>글쓴이</th>";
+  echo "    <td class='t1'>",$row['writer'],"</td>";
+  echo "  </tr>";
+  echo "  <tr>";
+  echo "    <th class='write'>조회수</th>";
+  echo "    <td class='t1'>",$row['hit'],"</td>";
+  echo "  </tr>";
+  echo "  <tr>";
+  echo "    <th class='write'>제목</th>";
+  echo "    <td class='t1'>",$row['title'],"</td>";
+  echo "  </tr>";
+  echo "  <tr>";
+  echo "    <th class='write'>내용</th>";
+  echo "    <td class='t1'>",$row['content'],"</td>";
+  echo "  </tr>";
+  echo "  <tr>";
+  echo "    <td colspan='2' class='btn'>";
+  echo "      <span id='span1'>";
+  echo "        <input type='button' class='btn1' value='글 수정' onclick='modify(",$num,")'>";
+  echo "        <input type='button' class='btn1' value='글 삭제' onclick='delview()'>";
+  echo "      </span>";
+  echo "      <form id='frm1' name='frm1' action='deleteProc.php' method='post' style='display:inline;display:none'>";
+  echo "        <input type='hidden' name='num' value='",$num,"'>";
+  echo "        비밀번호 : <input type='password' id='pwd' name='pwd' size='12' maxlength='12'>";
+  echo "        <input type='button' value='삭제' class='btn1' onclick='del()'>";
+  echo "        <input type='button' value='취소' class='btn1' onclick='cancel()'>";
+  echo "      </form>";
+  echo "    </td>";  
+  echo "  </tr>";
+  echo "</table>";
+
+  mysqli_close($conn);
+?>
+</body>
+</html>
